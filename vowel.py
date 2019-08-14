dda44964f6faadc845be3570cb3bca2ffa592050
Matheswarirevathi K <36396186+smartzmaddy@users.noreply.github.com>
2019-08-14 20:57:53 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -2,4 +2,4 @@ ch=input("enter a character:")
 if(ch==('a' or 'e' or 'i' or 'o' or 'u' or 'A' or 'E' or 'I' or 'O' or'U')):
    print(ch,"is vowel")
 else:
-   print(ch,"is constant")
+   print(ch,"is consonant")
